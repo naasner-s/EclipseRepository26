a27c40e913ec92a93abae9c958e043aeab63b499
Naasner <s.naasner@osztiem-edu.de>
2026-04-24 10:14:10 +0200

    StartCommit

---
+++ src/Grundfahrzeug.java
@@ -0,0 +1,6 @@
+
+public class  Grundfahrzeug {
+    String name;
+    String farbe;
+    int geschwindigkeit=0;
+}
---
+++ src/Strasse.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Strasse {
+
+	public static void main(String[] args) {
+		ArrayList<Grundfahrzeug> unsereFahrzeuge = new ArrayList<Grundfahrzeug>();
+		
+
+	}
+
+}
