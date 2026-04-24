90f0161074baa62b7c9358d50a0d552d2e0a9e56
Naasner <s.naasner@osztiem-edu.de>
2026-04-24 10:37:39 +0200

    Naasner hat Dreirad hinzugefügt

---
+++ src/Dreirad.java
@@ -0,0 +1,10 @@
+
+public class Dreirad extends Grundfahrzeug {
+   int anzahlRäder =3;
+   
+   Dreirad()
+   {
+	   farbe="blau";
+	   geschwindigkeit=100;
+   }
+}
--- src/Strasse.java
+++ src/Strasse.java
@@ -5,6 +5,7 @@ public class Strasse {
 	public static void main(String[] args) {
 		ArrayList<Grundfahrzeug> unsereFahrzeuge = new ArrayList<Grundfahrzeug>();
 		
+		unsereFahrzeuge.add(new Dreirad());
 
 	}
 
