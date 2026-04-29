8d73c7916784deca592b85536dcfa2d9baf74299
Adrian <adrian.noah.toll@osztiem-edu.de>
2026-04-29 11:02:37 +0200

    Mein Fenster ist Fertig

--- src/application/FensterTestController.java
+++ src/application/FensterTestController.java
@@ -1,7 +1,9 @@
 package application;
 
 import java.io.IOException;
-
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.scene.control.CheckBox;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,13 +16,34 @@ import javafx.stage.Stage;
 
 public class FensterTestController {
 	
+    @FXML
+    private CheckBox cbAdrian;
 	
+    @FXML
+    private Button btFensterAdrian;
 	
     @FXML
     private Button btDrücken;
 
     @FXML
     private TextField tfAnzeige;
+    
+    @FXML
+    void WaechselnFensterAdi(ActionEvent event) throws IOException 
+    {
+    	Stage stage3 = new Stage();
+    	FXMLLoader loaderAdrian = new FXMLLoader();
+    	loaderAdrian.setLocation(getClass().getResource("FensterAdrian.fxml"));
+    	Parent rootAdrian = loaderAdrian.load();
+    	FensterAdrianController controller3 = loaderAdrian.getController();
+    	stage3.setScene(new Scene(rootAdrian));
+    	stage3.setTitle("Das ist Adrian's Fenster");
+    	stage3.initModality(Modality.APPLICATION_MODAL);
+    	
+    	stage3.showAndWait();
+    	tfAnzeige.setText(controller3.getText());
+    	cbAdrian.setSelected(controller3.getBool());
+    }
 
     @FXML
     void drueckenSchaltflaeche(ActionEvent event) throws IOException 
--- src/application/NaasnerSeiteController.java
+++ src/application/NaasnerSeiteController.java
@@ -1,5 +1,6 @@
 package application;
 
+import javafx.scene.control.CheckBox;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
