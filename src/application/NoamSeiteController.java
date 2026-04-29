fcb1a8501eb8952b365b51aa1a0d83f2507c0b24
Noam <noam.toan.thang.vu@osztiem-edu.de>
2026-04-29 09:29:54 +0200

    Slider

--- src/application/NoamSeiteController.java
+++ src/application/NoamSeiteController.java
@@ -3,11 +3,18 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class NoamSeiteController {
-	
+	 @FXML
+	    private Button btSD;
+	 @FXML
+	    private TextField tfSD;
+	@FXML
+    private Slider sd;
+
 	String text;
     @FXML
     private Button btSchließen;
@@ -19,10 +26,20 @@ public class NoamSeiteController {
     void schließenFenster(ActionEvent event) {
     	Stage stage = (Stage) btSchließen.getScene().getWindow();
         stage.close();
-    	setText(tfText.getText());
+    	//setText(tfText.getText());
+    	setText(tfSD.getText());
+
+    }
+    @FXML
+    void sliderAnzeige(ActionEvent event) {
+    	tfSD.setText(String.valueOf(sd.getValue()));
+    	
     }
 
-    
+    String getText2()
+    {
+    	return tfText.getText();
+    }
 	public String getText() {
 		return text;
 	}
