f9d824e18bc3a194b788b0a20e1e7940e9dfc08e
Emirhan Özer <emirhan.oezer@osztiem-edu.de>
2026-04-29 21:29:23 +0200

    Video

--- src/application/EmirSeiteController.java
+++ src/application/EmirSeiteController.java
@@ -5,6 +5,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
 public class EmirSeiteController {
@@ -13,6 +16,8 @@ public class EmirSeiteController {
 
     @FXML
     private TextField tfText;
+    @FXML
+    private MediaView mediaView;
     
     @FXML
     String schließenFenster(ActionEvent event) {
@@ -27,6 +32,15 @@ public class EmirSeiteController {
     {
     	return tfText.getText();
     }
-
+    public void initialize() {
+    	String path = getClass().getResource("Videos/dombora.mp4").toExternalForm();
+    	
+    	Media media = new Media(path);
+    	MediaPlayer player = new MediaPlayer(media);
+    	mediaView.setMediaPlayer(player);
+    	player.play();
+    	
+    }
+    
 
 }
