9cc5d7adac1b8088d42585c80d562350b154aad6
Omar <omar.mohammad.nejem@osztiem-edu.de>
2026-04-29 11:02:54 +0200

    OmarCommit2

--- src/application/FensterTestController.java
+++ src/application/FensterTestController.java
@@ -40,6 +40,21 @@ public class FensterTestController {
         tfAnzeige.setText(controller2.getText()); //Holt aus dem schließenden Fenster den Wert
             	
 	}
+    void drueckenSchaltflaeche2(ActionEvent event) throws IOException 
+    {
+    	Stage stage2 = new Stage();
+    	FXMLLoader loader = new FXMLLoader();
+    	loader.setLocation(getClass().getResource("OmarSeite.fxml"));
+    	Parent root = loader.load();
+        NaasnerSeiteController controller2 = loader.getController();  //Braucht man nur, wenn man Werte aus dem neuen Fenster zurückgeben will
+        stage2.setScene(new Scene(root));
+        stage2.setTitle("Das ist das 2. Fenster");
+        stage2.initModality(Modality.APPLICATION_MODAL); // Dann kann das alte Fenster nicht mehr aktiv sein
+      
+        stage2.showAndWait();
+        tfAnzeige.setText(controller2.getText()); //Holt aus dem schließenden Fenster den Wert
+            	
+	}
  }
 
 
