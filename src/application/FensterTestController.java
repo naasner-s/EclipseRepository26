6aaed121b1b6a5d98de92d97309bbf6ea6c12e04
KJ <amrene.gmail.com>
2026-05-12 21:33:50 +0200

    Gefixt oder so

--- src/application/FensterTestController.java
+++ src/application/FensterTestController.java
@@ -39,18 +39,19 @@ public class FensterTestController {
         
             	
 	}  
+    @FXML
     void druckenrückgabeAmr() throws IOException {
     	Stage stage2 = new Stage();
     	FXMLLoader loader = new FXMLLoader();
-    	loader.setLocation(getClass().getResource("NaasnerSeite.fxml"));
+    	loader.setLocation(getClass().getResource("AmrSeite.fxml"));
     	Parent root = loader.load();
         AmrSeiteController controller2 = loader.getController();  //Braucht man nur, wenn man Werte aus dem neuen Fenster zurückgeben will
         stage2.setScene(new Scene(root));
-        stage2.setTitle("Das ist das 2. Fenster");
+        stage2.setTitle("Das ist das 1. Fenster");
         stage2.initModality(Modality.APPLICATION_MODAL); // Dann kann das alte Fenster nicht mehr aktiv sein
       
         stage2.showAndWait();
-     //   tfAnzeige.setText(controller2.getText()); //Holt aus dem schließenden Fenster den Wert
+        tfAnzeige.setText(controller2.getText()); //Holt aus dem schließenden Fenster den Wert
         
     	
     		
